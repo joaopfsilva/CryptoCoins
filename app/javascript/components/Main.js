fb212b2f8ee98fd9b337bf47105e6767219652c9
João Pedro Ferreira da Silva <j0a0.silva.kle@gmail.com>
2019-02-04 17:05:26 +0100

    basic configuration and dummy component example

---
+++ app/javascript/components/Main.js
@@ -0,0 +1,16 @@
+import React from "react"
+import PropTypes from "prop-types"
+import CoinList from './CoinList'
+
+class Main extends React.Component {
+  render () {
+    return (
+    	<div>
+    	lskdjflsdhlkdsjlfj
+    	ajdhaksh
+    	</div>
+    );
+  }
+}
+
+export default Main
